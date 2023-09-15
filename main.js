7a94426e3d036ac2fd0779c36d7530ae84eb4e8d
Al-Mumeetu Saikat <113462809+almumeetu@users.noreply.github.com>
2023-09-15 15:53:58 +0600

    working on about section

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-console.log("lllllllllllllllllllllllllllllllll");
+const header = document.querySelector("header");
+
+window.addEventListener ("scroll", function(){
+    header.classList.toggle ("sticky", window.scrollY > 120);
+});
+
+console.log("what's problem.....??");
